57be86d8c773b76a0cec6743a72e4fb080888794
marcelo <marcelofavaroborges@gmail.com>
2022-08-18 15:31:11 -0300

    feat: initial commit

---
+++ delfos-frontend-test/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button, Typography } from "@mui/material";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Typography mt={2}> Initial </Typography>
+    </div>
+  );
+};
+
+export default Home;
